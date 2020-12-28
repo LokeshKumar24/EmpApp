d9ff622fb5c30080524e82025e91b58ab4bf359f
AnjumNadaf <anjunadaf7@gmail.com>
2020-12-28 12:50:01 +0000

    Seventh

--- webapp/controller/Login.controller.js
+++ webapp/controller/Login.controller.js
@@ -9,18 +9,88 @@ sap.ui.define([
 		"use strict";
 
 		return Controller.extend("EA.EmployeeApp1.controller.Login", {
+            
+            
 			onInit: function () {
-               this.oRouter = new sap.ui.core.UIComponent.getRouterFor(this);
-               this.getView().setModel(new JSONModel(),"ch");
-                this.getView().setModel(new JSONModel(),"login");
+                this.oRouter = new sap.ui.core.UIComponent.getRouterFor(this);
+                var that = this;
+                var OData;
+                var oJSONModel = new JSONModel();
+                this.getView().setModel(oJSONModel, "jsonmodel");
+             
+                 
+                
+                
+                // var sUrl = new sap.ui.model.odata.ODataModel("/sap/opu/odata/sap/ZAPP_EMP_SRV/");
+                var oModel = new sap.ui.model.odata.ODataModel("/sap/opu/odata/sap/ZAPP_EMP_SRV/");
+            var data=oModel.read("/LOGINSet", {
+                success:function(data1){
+                    debugger;
+                   // alert("success");
+                    OData= data.results;
+                    console.log(data.results); 
+                    that.getView().setModel(new JSONModel({LoginData: data.results}),"jsonmodel");
+                   
+                    
+                    // set the model
+                    //  that.masterData(OData);  
+                                  
+                },
+                error:function(){
+                    alert("error");
+                }
+            });
 
+                
+                
+
+               
+            //    this.getView().setModel(new JSONModel(),"ch");
+            //     this.getView().setModel(new JSONModel(),"login");
+
+            },
+
+
+             masterData : function(OData){
+             debugger;
+             var arr = [];
+             arr.push(data1.results);
+            //  console.log(oData1);
+             var oJSONModel = new JSONModel();
+             
+
+             oJSONModel.setData({
+					LoginData: OData
+                });
+                
+                this.getView().setModel(oJSONModel, "jsonmodel");
+                // var detail = this.getView().getModel("jsonmodel").getProperty("/data");
+                // console.log(detail);
+                // var idE3 = detail[0].Eid;
+                // console.log(idE3);
+               
+               
             },
             onLogin:function(){
                 debugger;
-                var id = this.getView().getModel("login").getProperty("/id");
-                var Password = this.getView().getModel("login").getProperty("/password");
+                // var id = this.getView().getModel("login").getProperty("/id");
+                // var Password = this.getView().getModel("login").getProperty("/password");
 
-                this.oRouter.navTo("SplitApp");
+                var id = this.getView().byId("eid").getValue();
+                var password = this.getView().byId("epass").getValue();
+
+                var oModel = this.getView().getModel("jsonmodel").getProperty("/LoginData");
+                for (var i=0; i<=oModel.length; i++){
+                    if(oModel[i].Eid === id && oModel[i].Password === password){
+                         this.oRouter.navTo("SplitApp");
+                    }else{
+                        // sap.m.MessageToast.show("Please Enter Login Credentials");
+                        alert("ERROR");
+                    }
+
+                }
+
+                
             },
             changePass:null,
             changePassword:function(){
