7eb63579b9817df5f3d874fe9b81bc9e5a1cd220
LokeshKumar24 <Lokeshkumar24021996@gmail.com>
2020-12-28 08:20:12 +0000

    master detail

--- webapp/controller/Login.controller.js
+++ webapp/controller/Login.controller.js
@@ -1,22 +1,46 @@
 sap.ui.define([
-		"sap/ui/core/mvc/Controller"
+        "sap/ui/core/mvc/Controller",
+        "sap/ui/model/json/JSONModel"
 	],
 	/**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-	function (Controller) {
+	function (Controller,JSONModel) {
 		"use strict";
 
 		return Controller.extend("EA.EmployeeApp1.controller.Login", {
 			onInit: function () {
                this.oRouter = new sap.ui.core.UIComponent.getRouterFor(this);
+               this.getView().setModel(new JSONModel(),"ch");
+                this.getView().setModel(new JSONModel(),"login");
+
             },
             onLogin:function(){
                 debugger;
-                var id = this.getView().byId("idUsername").getValue();
-                var Password = this.getView().byId("idPassword").getValue();
+                var id = this.getView().getModel("login").getProperty("/id");
+                var Password = this.getView().getModel("login").getProperty("/password");
 
-                this.oRouter.navTo("RouteHome");
+                this.oRouter.navTo("SplitApp");
+            },
+            changePass:null,
+            changePassword:function(){
+               // debugger;
+                if(!this.changePass){
+                    this.changePass = new sap.ui.xmlfragment("EA.EmployeeApp1.view.changePassword",this);
+                    this.getView().addDependent(this.changePass);
+                }
+                this.changePass.open();
+
+            },
+            onClose:function(){
+                this.changePass.close();
+            },
+            newPassword:function(){
+                debugger
+             var id = this.getView().getModel("ch").getProperty("/id");
+			var newPass = this.getView().getModel("ch").getProperty("/newPass");
+            var confPass = this.getView().getModel("ch").getProperty("/confPass");
+            sap.m.MessageToast.show("Password updated Successfully");
             }
 
 		});
--- webapp/controller/SplitApp.controller.js
+++ webapp/controller/SplitApp.controller.js
@@ -1,15 +1,53 @@
 sap.ui.define([
-		"sap/ui/core/mvc/Controller"
+        "sap/ui/core/mvc/Controller",
+        "sap/ui/model/json/JSONModel",
+        	"sap/base/Log"
 	],
 	/**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-	function (Controller) {
+	function (Controller,JSONModel,Log) {
 		"use strict";
 
 		return Controller.extend("EA.EmployeeApp1.controller.SplitApp", {
 			onInit: function () {
 
-			}
+                var oViews=["Home","Profile","Project","Others"];
+                var views=[];
+                oViews.forEach(element=>{
+                views.push({name:element})
+                });
+              var data={
+                         views: views
+                      }
+                this.getView().setModel(new JSONModel(data),"master")
+            },
+            request:null,
+            onRequest:function(){
+                debugger
+                if(!this.request){
+                    this.request = new sap.ui.xmlfragment("EA.EmployeeApp1.view.Request",this);
+                    this.getView().addDependent(this.request);
+                }
+                this.request.open();
+
+            },
+            onClose:function(){
+                this.request.close();
+            },
+            	onOrientationChange: function (oEvent) {
+			var bLandscapeOrientation = oEvent.getParameter("landscape"),
+				sMsg = "Orientation now is: " + (bLandscapeOrientation ? "Landscape" : "Portrait");
+			sap.m.MessageToast.show(sMsg, { duration: 5000 });
+		},
+
+            	onListItemPress: function (oEvent) {
+                  //  debugger
+            var sToPageId = oEvent.getParameter("listItem").mProperties.title;
+
+			this.byId("SplitApp").toDetail(this.createId(sToPageId));
+		}
+
+            
 		});
 	});
