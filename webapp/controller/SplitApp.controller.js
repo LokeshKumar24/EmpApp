e29016d10a90637edf3061d5e078b597ab22188c
AnjumNadaf <anjunadaf7@gmail.com>
2020-12-30 13:28:47 +0000

    Edit i profile view

--- webapp/controller/SplitApp.controller.js
+++ webapp/controller/SplitApp.controller.js
@@ -15,6 +15,8 @@ sap.ui.define([
 
             oRouter.attachRoutePatternMatched(this.getId, this);
             //  this.getProfile();
+
+            
             
         },
         
@@ -84,7 +86,102 @@ sap.ui.define([
                   this.getOwnerComponent().setModel(new JSONModel({profile:detail}),"profileModel")
                     this.getView().byId("ObjectPageLayout").bindElement("profileModel>/profile/"+path);
                 }
-            }
+            },
+
+            onOpenEdit:function(){
+                debugger;
+                var sForm = this.getView().byId("SmForm1");
+                sForm.setVisible(false);
+
+                 this.getView().byId("SmForm6").setVisible(false);
+
+                var form = this.getView().byId("SimpleFormChange354");
+                form.setVisible(true);
+
+
+                var edit = this.getView().byId("edit");
+                edit.setVisible(false);
+
+                this.getView().byId("save").setVisible(true);
+                this.getView().byId("cancel").setVisible(true);
+           },
+
+           handleCancelPress:function(){
+               debugger;
+
+               var form = this.getView().byId("SimpleFormChange354");
+                form.setVisible(false);
+
+                var sForm = this.getView().byId("SmForm1");
+                sForm.setVisible(true);
+
+                 var edit = this.getView().byId("edit");
+                edit.setVisible(true);
+
+                this.getView().byId("save").setVisible(false);
+                this.getView().byId("cancel").setVisible(false);
+
+                 this.getView().byId("SmForm6").setVisible(true);
+
+               
+
+
+
+           },
+           handleSavePress:function(){
+               debugger;
+               var IntId = this.getView().byId("idInput").getValue();
+               var IntName = this.getView().byId("nameInput").getValue();
+               var IntPno = this.getView().byId("noInput").getValue();
+               var IntAge = this.getView().byId("ageInput").getValue();
+               var IntAdress = this.getView().byId("addInput").getValue();
+               var IntCity = this.getView().byId("cityInput").getValue();
+
+               
+
+
+             var serviceurl="/sap/opu/odata/sap/ZAPP_EMP_SRV/";
+
+             var oPModel =  new sap.ui.model.odata.ODataModel(serviceurl);
+
+             var Payload = {};
+
+             Payload.Eid = IntId;
+             Payload.Fullname = IntName;
+             Payload.Phoneno = IntPno;
+             Payload.Age = IntAge;
+             Payload.Address = IntAdress;
+             Payload.City = IntCity ;
+
+              oPModel .update("/PROFILESet('"+Payload.Eid+"')", Payload, {
+                     method: "PUT",
+                     success: function(data) {
+                    //   alert("success");
+                    sap.m.MessageToast.show("Updated Succesfully");
+                    },
+                     error: function(e) {
+                      alert("error");
+                    }
+                });
+
+                this.getView().byId("SmForm1").setVisible(true);
+
+                var form = this.getView().byId("SimpleFormChange354");
+                form.setVisible(false);
+
+                 this.getView().byId("SmForm6").setVisible(true);
+
+                  
+                  this.getView().byId("edit").setVisible(true);
+                 this.getView().byId("save").setVisible(false);
+                 this.getView().byId("cancel").setVisible(false);
+
+
+
+
+          
+               
+           }
 
             
 		});
